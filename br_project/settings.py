09ccc46188bb9427f77aa0524c8f27f21860242d
Bob Rockefeller <bob@bobrockefeller.com>
2025-02-12 08:17:21 -0500

    Tweaking for pythonanywhere

--- br_project/settings.py
+++ br_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-whe=k#(eun53l5n5*d*^)_)b-64que=1b6_df4=*5k47$)d5%9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
